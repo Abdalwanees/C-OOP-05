9dbce8adfed71ab292b281ebe5714c0a9866fcf4
Abdalwanees <gabdo7682@gmail.com>
2024-07-23 12:16:01 +0300

    Operator overloading (Unary Operators , Ternary Operator ,Relational Operators)

---
+++ Demo/Operator overloading/Complex.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Operator_overloading
+{
+    internal class Complex
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i\nHashcode {GetHashCode()}";
+        }
+        //To overload operator
+        //       -->Function Must be non private  
+        //       -->Function Must be static 
+        public static Complex operator + (Complex left, Complex right)
+        {
+            return new Complex
+            {
+                Real =( left?.Real ?? 0) + (right.Real),//--> ?? Nullcoleasing operator
+                Imag = (left?.Imag ?? 0) + (right.Imag)//--> ?? Nullcoleasing operator
+            };
+
+        }
+        public static Complex operator - (Complex left, Complex right)
+        {
+            return new Complex
+            {
+                Real =( left?.Real ?? 0) - (right.Real),//--> ?? Nullcoleasing operator
+                Imag = (left?.Imag ?? 0) - (right.Imag)//--> ?? Nullcoleasing operator
+            };
+
+        }
+        public static Complex operator ++(Complex comp)
+        {
+            return new Complex { Real = comp?.Real??0 + 1, Imag = comp?.Imag??0 + 1 };
+        }
+        public static bool operator >(Complex Left, Complex Right) {
+            return (Left.Real == Right.Real) && (Left?.Imag > Right?.Imag) || (Left?.Real > Right?.Real);
+        //    if (Left?.Real==Right?.Real)
+        //    {
+        //        return Left?.Imag > Right?.Imag;
+        //    }
+        //    else
+        //    {
+        //        return Left?.Real > Right?.Real;
+        //    }
+        }
+        public static bool operator <(Complex Left, Complex Right) {
+            return (Left.Real == Right.Real) && (Left?.Imag < Right?.Imag) || (Left?.Real < Right?.Real);
+        //    if (Left?.Real==Right?.Real)
+        //    {
+        //        return Left?.Imag < Right?.Imag;
+        //    }
+        //    else
+        //    {
+        //        return Left?.Real < Right?.Real;
+        //    }
+        }
+        public static bool operator<=(Complex Left, Complex Right) {
+            return (Left.Real == Right.Real) && (Left?.Imag <= Right?.Imag) || (Left?.Real <= Right?.Real);
+            //    if (Left?.Real==Right?.Real)
+            //    {
+            //        return Left?.Imag <= Right?.Imag;
+            //    }
+            //    else
+            //    {
+            //        return Left?.Real <= Right?.Real;
+            //    }
+        }
+        public static bool operator>=(Complex Left, Complex Right) {
+            return (Left.Real == Right.Real) && (Left?.Imag >= Right?.Imag) || (Left?.Real >= Right?.Real);
+            //    if (Left?.Real==Right?.Real)
+            //    {
+            //        return Left?.Imag >= Right?.Imag;
+            //    }
+            //    else
+            //    {
+            //        return Left?.Real >= Right?.Real;
+            //    }
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,69 @@
+﻿using Demo.Operator_overloading;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Operator overloading (Unary Operators , Ternary Operator ,Relational Operators)
+            #region Operator Overloading (+,-,*,/)
+            ////complex numbre (realnum&imaginnum)
+            //Complex c1 = new Complex() { Real = 3, Imag = 4 };
+            //Complex c2 = new Complex() { Real = 1, Imag = 2 };
+
+            ////Complex c3 =c1+c2 ;--> not correct 
+            //Complex c3 = new Complex();
+            //Console.WriteLine("------------------");
+            //Console.WriteLine(c1);
+            //Console.WriteLine(c2);
+            //Console.WriteLine(c3);
+            //Console.WriteLine("------------------");
+
+            //c3 += c1;
+            //Console.WriteLine(c3.ToString());
+            //c3 -= c2;
+            //Console.WriteLine(c3.ToString());
+
+            #endregion
+            #region Operator Overloading _-->Ternary
+            //c3 = c1++;
+            //Console.WriteLine(c3.ToString());
+            //c3 = ++c1;
+            //Console.WriteLine(c3.ToString());
+            #endregion
+            #region operator overloadingre -->Reletional operator
+            //if (c1 > c2 && c1 > c3)
+            //{
+            //    Console.WriteLine("C1 is Greater");
+            //}
+            //else if (c2 > c1 && c2 > c3)
+            //{
+            //    Console.WriteLine("C2 is Greater");
+            //}
+            //else if (c3 > c1 && c3 > c2)
+            //{
+            //    Console.WriteLine("C3 is Greater");
+            //}
+            //else if (c1 == c2 && c1 > c3)
+            //{
+            //    Console.WriteLine("C1 and C2 are equal and greater than C3");
+            //}
+            //else if (c1 == c3 && c1 > c2)
+            //{
+            //    Console.WriteLine("C1 and C3 are equal and greater than C2");
+            //}
+            //else if (c2 == c3 && c2 > c1)
+            //{
+            //    Console.WriteLine("C2 and C3 are equal and greater than C1");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("All values are equal");
+            //}
+
+            #endregion 
+            #endregion
+        }
+    }
+}
